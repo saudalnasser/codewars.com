88f94f72b1853705776c60933c8e491bb8c3fc73
saud alnasser <saud4services@gmail.com>
2022-10-26 00:05:59 +0300

    solve https://www.codewars.com/kata/54ff3102c1bad923760001f3

---
+++ solutions/7kyu/54ff3102c1bad923760001f3.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3
+
+export class Kata {
+  public static getCount(str: string): number {
+    const vowels = "aeiou";
+    return [...str].reduce((count, letter) => vowels.includes(letter) ? count + 1: count, 0)
+  }
+}
