cf2a777e579c3842067589d324d6a34add5cdbf6
saud alnasser <saud4services@gmail.com>
2022-10-25 23:03:54 +0300

    remove eslint style enforcement

--- solutions/53da3dbb4a5168369a0000fe.ts
+++ solutions/53da3dbb4a5168369a0000fe.ts
@@ -1,6 +1,5 @@
 // https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
 
-// eslint-disable-next-line camelcase
 export function even_or_odd(n: number): string {
   return n % 2 === 0 ? 'Even' : 'Odd';
 }
